1bd58b824456b9304f2888ed651e28899b16be83
Liu Lixiang <liulixiang1988@gmail.com>
2019-07-28 17:19:43 +0800

    141-环形链表检测

--- 141-LinkedListCycle/src/Solution.java
+++ 141-LinkedListCycle/src/Solution.java
@@ -54,4 +54,25 @@ public class Solution {
         }
         return false;
     }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while(head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 }
