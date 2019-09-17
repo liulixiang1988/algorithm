a0db2cebbb5cde56c1e4f6424f11cedcd62140a1
liulixiang1988 <liulixiang1988@gmail.com>
2019-09-17 08:27:48 +0800

    98. 验证二叉搜索树

---
+++ lc/src/test/java/lc98/SolutionTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Liu Lixiang 2019-2019. All rights reserved
+ */
+
+package lc98;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO Class Description
+ *
+ * @since 2019-09-17
+ */
+public class SolutionTest {
+
+  @Test
+  void isValidBST() {
+    Solution solution = new Solution();
+    solution.isValidBST(null);
+  }
+}
