56636252829240ceb090e9a08665f67230c7e734
liulixiang1988 <liulixiang1988@gmail.com>
2019-10-27 11:58:42 +0800

    94.二叉树的中序遍历

---
+++ lc_1/94.二叉树的中序遍历.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (68.57%)
+ * Likes:    309
+ * Dislikes: 0
+ * Total Accepted:    70.1K
+ * Total Submissions: 102.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的中序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * 输出: [1,3,2]
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+    }
+
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+    }
+}
+// @lc code=end
+
