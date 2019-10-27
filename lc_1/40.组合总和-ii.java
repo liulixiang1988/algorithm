fdad84b10e1109859a8233719af4397b12b03c7f
liulixiang1988 <liulixiang1988@gmail.com>
2019-10-27 09:12:30 +0800

    40不重复组合总和

---
+++ lc_1/40.组合总和-ii.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=40 lang=java
+ *
+ * [40] 组合总和 II
+ *
+ * https://leetcode-cn.com/problems/combination-sum-ii/description/
+ *
+ * algorithms
+ * Medium (57.41%)
+ * Likes:    159
+ * Dislikes: 0
+ * Total Accepted:    26.3K
+ * Total Submissions: 45.8K
+ * Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+ *
+ * 给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * 
+ * candidates 中的每个数字在每个组合中只能使用一次。
+ * 
+ * 说明：
+ * 
+ * 
+ * 所有数字（包括目标数）都是正整数。
+ * 解集不能包含重复的组合。 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+ * 所求解集为:
+ * [
+ * ⁠ [1, 7],
+ * ⁠ [1, 2, 5],
+ * ⁠ [2, 6],
+ * ⁠ [1, 1, 6]
+ * ]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: candidates = [2,5,2,1,2], target = 5,
+ * 所求解集为:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        backtrack(list, tempList, candidates, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain == 0) {
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+        if (remain < 0) {
+            return;
+        }
+
+        for(int i = start; i < nums.length; i++) {
+            // 去重
+            if (i > start && nums[i] == nums[i-1]) {
+                continue;
+            }
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+// @lc code=end
