80c8e5b1e121d255198e3722b7831d04f4b9b82b
liulixiang1988 <liulixiang1988@gmail.com>
2019-10-27 09:55:31 +0800

    90子集-回溯

---
+++ lc_1/90.子集-ii.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=90 lang=java
+ *
+ * [90] 子集 II
+ *
+ * https://leetcode-cn.com/problems/subsets-ii/description/
+ *
+ * algorithms
+ * Medium (57.34%)
+ * Likes:    136
+ * Dislikes: 0
+ * Total Accepted:    15.1K
+ * Total Submissions: 26.3K
+ * Testcase Example:  '[1,2,2]'
+ *
+ * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,2,2]
+ * 输出:
+ * [
+ * ⁠ [2],
+ * ⁠ [1],
+ * ⁠ [1,2,2],
+ * ⁠ [2,2],
+ * ⁠ [1,2],
+ * ⁠ []
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        // 回溯
+        backtrack(list, tempList, nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<Integer>(tempList));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i+1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+// @lc code=end
+
