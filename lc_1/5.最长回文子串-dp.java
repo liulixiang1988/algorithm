b5fc235c14abce086bf9bddf58c2c718d9636b05
liulixiang1988 <liulixiang1988@gmail.com>
2020-02-18 07:49:47 +0800

    [5] 最长回文子串

---
+++ lc_1/5.最长回文子串-dp.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (28.50%)
+ * Likes:    1756
+ * Dislikes: 0
+ * Total Accepted:    182.4K
+ * Total Submissions: 640K
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 
+ * 示例 1：
+ * 
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        String result = "";
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] && j - i + 1 > result.length()) {
+                    result = s.substring(i, j + 1);
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
